cd756cc9e46bcc5f6f20c907b18483f4ab6a3d5d
Oleksandra Flomboim <sflomboim@gmail.com>
2026-03-14 10:12:22 +0200

    feat: add OrderItem class

---
+++ src/main/java/OrderItem.java
@@ -0,0 +1,52 @@
+import java.util.Objects;
+
+public class OrderItem {
+
+    private final int id;
+    private String name;
+    private double price;
+
+    public OrderItem(int id, String name, double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "OrderItem: id = '%d', name = '%s', price = '%d'".formatted(id, name, price);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if(this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        OrderItem orderItem = (OrderItem) o;
+        return id == orderItem.id && Double.compare(price, orderItem.price) == 0 && Objects.equals(name, orderItem.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, price);
+    }
+}
