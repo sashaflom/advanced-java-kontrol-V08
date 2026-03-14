8e015d90fd77a95c4fb1db352932cc36c2759685
Oleksandra Flomboim <sflomboim@gmail.com>
2026-03-14 11:19:44 +0200

    findById

--- src/main/java/OrderService.java
+++ src/main/java/OrderService.java
@@ -1,2 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 public class OrderService {
+
+    private final List<Order> repository = new ArrayList<>();
+
+    public Optional<Order> findById(int id) {
+        return repository.stream()
+                .filter(o -> o.getId() == id)
+                .findFirst();
+    }
+
 }
