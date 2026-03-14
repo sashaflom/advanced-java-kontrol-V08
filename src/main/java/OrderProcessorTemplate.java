1e51761a62ca39d0e94150a81a024f6845991ccf
Oleksandra Flomboim <sflomboim@gmail.com>
2026-03-14 11:00:09 +0200

    feat: add code to calculatePrice method

--- src/main/java/OrderProcessorTemplate.java
+++ src/main/java/OrderProcessorTemplate.java
@@ -1,18 +1,29 @@
 public abstract class OrderProcessorTemplate {
 
-    public final void process(Order order) {
-        validateOrder(order);
-        fraudCheck(order);
-        calculatePrice(order);
-        pay(order);
-        sendNotification(order);
+    public final void process(Order order) throws AppException{
+        try{
+            validateOrder(order);
+            if (fraudCheck(order)) {
+                calculatePrice(order);
+                pay(order);
+                sendNotification(order);
+            }else{
+                throw new FraudCheckFailedException("Fraud was detected.");
+            }
+
+        }catch(Exception e){
+            throw new AppException("Order processing failed", e);
+        }
+
     }
 
     protected abstract void validateOrder(Order order);
     protected abstract boolean fraudCheck(Order order);
 
     protected void calculatePrice(Order order){
-
+        if (order.getTotalPrice() > 20000) {
+            // make a discount
+        }
     }
 
     protected abstract void pay(Order order);
