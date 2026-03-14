472af0030f7472678fdba57d480646c37e71e7a2
Oleksandra Flomboim <sflomboim@gmail.com>
2026-03-14 10:22:48 +0200

    feat: add Order class

---
+++ src/main/java/Order.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+public class Order {
+
+    private final int id;
+    private OrderItem[] items;
+    private double totalPrice;
+
+    public Order(int id, OrderItem[] items) {
+        this(id, items, 0.0);
+    }
+
+    public Order(int id, OrderItem[] items, double totalPrice) {
+        this.id = id;
+        this.items = items != null ? Arrays.copyOf(items, items.length) : new OrderItem[0];
+        this.totalPrice = totalPrice;
+    }
+
+    public OrderItem[] getItems() {
+        return Arrays.copyOf(items, items.length);
+    }
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+}
